a2bfd573bd5b27ceff2a1f1a9926c013e26859db
ssi285 <ssi285@NOISSI28531385>
2017-06-15 12:52:23 +0530

    Sample Api is created for the test Excpetion, logging and security in with API project.

---
+++ Survey.Web.API/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace Survey.Web.API.Controllers
+{
+    [RoutePrefix("survey")]
+    public class SampleController : ApiController
+    {
+        [HttpGet]
+        [Route("GetRecord")]
+        public IHttpActionResult FetchData()
+        {
+            List<string> list = new List<string>();
+            list.Add("Ritesh");
+            list.Add("Ranjan");
+            return Ok(list);
+        }
+    }
+}
